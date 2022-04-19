4a38cdc8881dc8ca984ac8622ab83690d651c468
haidn <haidn@relipasoft.com>
2022-04-19 15:36:18 +0700

    fix bien moi truong

--- todolist/src/api/axiosClient.js
+++ todolist/src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosClient = axios.create({
-  baseURL: "https://62562aea52d8738c692c7cf5.mockapi.io/api/",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
--- todolist/src/config/firebase.js
+++ todolist/src/config/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase/compat/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARCGJc-mXpj6hBN3hggspKPfKCkMPqODs",
-  authDomain: "myapp-221907.firebaseapp.com",
-  projectId: "myapp-221907",
-  storageBucket: "myapp-221907.appspot.com",
-  messagingSenderId: "412953171914",
-  appId: "1:412953171914:web:ffc8cab28af04cfa086133",
-  measurementId: "G-QGK7J0D8Q0",
+  apiKey: process.env.REACT_APP_FIREBASE_API,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
